881dde324f498d2e2ae9625b798482a2053d09ea
DouglasFuelber <dffuelber@gmail.com>
2020-01-17 13:06:14 -0300

    Added nodemon

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send('Hello World');
+    return response.json({ message: 'Hello Teste' });
 });
 
 app.listen(3333);
