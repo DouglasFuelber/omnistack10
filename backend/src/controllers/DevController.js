a397b6d1471bbe26f8d3dc7e2a3b03d07daa4dfa
DouglasFuelber <dffuelber@gmail.com>
2020-01-18 15:33:07 -0300

    Fix devs controller to avoid multiples devs with same github_username

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,24 +5,28 @@ module.exports = {
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
 
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-        const { name = login, avatar_url, bio } = apiResponse.data;
+        let dev = await Dev.findOne({ github_username });
 
-        const techsArray = techs.split(',').map(tech => tech.trim());
-
-        const location = {
-            type: 'Point',
-            coordinates: [latitude, longitude]
-        }
-
-        const dev = await Dev.create({
-            github_username,
-            avatar_url,
-            name,
-            bio,
-            techs: techsArray,
-            location
-        });
+        if (!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+            const { name = login, avatar_url, bio } = apiResponse.data;
+    
+            const techsArray = techs.split(',').map(tech => tech.trim());
+    
+            const location = {
+                type: 'Point',
+                coordinates: [latitude, longitude]
+            }
+    
+            dev = await Dev.create({
+                github_username,
+                avatar_url,
+                name,
+                bio,
+                techs: techsArray,
+                location
+            });
+        }        
 
         return response.json(dev);
     }
