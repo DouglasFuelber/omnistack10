b56ae1edbf98f596407bb0cd7e25cf78fe4df0c2
DouglasFuelber <dffuelber@gmail.com>
2020-01-30 15:56:17 -0300

    Added market and callout to main page map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 export default () => {
@@ -33,11 +33,43 @@ export default () => {
         return null;
     }
 
-    return <MapView initialRegion={cureentRegion} style={styles.map} />
+    return <MapView initialRegion={cureentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -29.7269849, longitude: -52.5019521 }}>
+                <Image style={styles.avatar} source={{uri: 'https://avatars0.githubusercontent.com/u/15067098?s=460&v=4'}} />
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Douglas Fuelber</Text>
+                        <Text style={styles.devBio}>Full Stack Web Developer</Text>
+                        <Text style={styles.devTechs}>ReactJS, JS, C#, HTML, CSS</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1,
     },
+    avatar: {
+        borderColor: '#fff',
+        borderRadius: 4,
+        borderWidth: 3,
+        height: 50,
+        width: 50,
+    },
+    callout: {
+        width: 260
+    },
+    devName: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5
+    },
+    devTechs: {
+        marginTop: 5
+    }
 });
