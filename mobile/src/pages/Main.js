bc4692870c57a2afb990262330387e424b5a5c6a
DouglasFuelber <dffuelber@gmail.com>
2020-01-30 15:38:28 -0300

    Added map to main page

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-export default () => (
-    <View/>
-)
+export default () => {
+
+    const [cureentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        const loadInitialPosition = async () => {
+            const { granted } = await requestPermissionsAsync();
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                });
+            }
+        };
+
+        loadInitialPosition();
+
+    }, []);
+
+    if (!cureentRegion) {
+        return null;
+    }
+
+    return <MapView initialRegion={cureentRegion} style={styles.map} />
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+    },
+});
